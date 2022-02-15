ac122a976c8c860a6fe5cc6bc6aa6beed9f31ff4
lmanuel34 <56777883+lmanuel34@users.noreply.github.com>
2022-02-15 08:03:55 -0600

    game init

--- game.js
+++ game.js
@@ -1,1 +1,43 @@
+import player, {Player} from './player'
+
+class game extends Player {
+    
+    state = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]; 
+    board
+    timer = 0
+    constructor () {
+
+    }
+
+    setBoard() {
+       this.board = Array(5).fill(Array(5));  
+    }
+
+    getBoard () {
+        return this.board
+    }
+
+    setState(index) {
+        this.state[index] = this.playerType
+    }
+
+    getState() {
+        return this.state
+    }
+
+    setTimer(timer) {
+        this.timer = timer
+    }
+
+    getTime() {
+        return this.timer
+    }
+
+    gameRestart() {
+        this.state = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+        this.setBoard()
+        this.setTimer(0)
+    
+    }
+}
 
