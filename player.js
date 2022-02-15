549fbf1204dfe24d336707fdd314787308a8998d
lmanuel34 <56777883+lmanuel34@users.noreply.github.com>
2022-02-15 08:03:21 -0600

    player init

--- player.js
+++ player.js
@@ -1,1 +1,40 @@
+export default class player {
+    playerNumber = 0
+    playerType = 0
+    move = Array(5).fill(Array(5));
+    timeOut = 0
 
+    constructor (playerNumber, playerType) {
+        this.playerNumber = playerNumber
+        this.playerType = playerType
+    }
+
+    getPlayerNumber() {
+        return this.playerNumber;
+
+    }
+
+    getPlayerType() {
+        return this.playerType
+    }
+
+    setTimeout(timeOut) {
+        this.timeout = timeOut
+    }
+
+    getTimeOut(){
+        return this.timeOut
+    }
+
+    setMove(move, i, j){
+        this.move[i][j] = move
+    }
+
+    getMove(){
+        return this.move
+    }
+
+    playerChage(type) {
+        this.playerType = type
+    }
+}
