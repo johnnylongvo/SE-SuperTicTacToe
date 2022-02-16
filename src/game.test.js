367c9b7ab6e802a42e4866c163a73fdf281c4b2d
Brent Bashaw <bbashaw@uco.edu>
2022-02-16 10:54:49 -0600

    notes for game.test.js

--- src/game.test.js
+++ src/game.test.js
@@ -1,7 +1,11 @@
 // we need to install enumifyjs, use typescript, or homebrew an enum class
+// the following line gets the game class
 const sum = require('./game');
+// this is an array defining potential values for a game square
 let xando = ["x","o",""];
+// this pulls a random value from the above array
 var rando = xando[Math.random(Math.floor()*xando.length)];
+// this defines a game whose state should not be possible
 let extraneous = [
     ["x","x","x","x","x"],
     ["o","o","o","o","o"],
@@ -9,6 +13,7 @@ let extraneous = [
     ["o","o","o","o","o"],
     ["x","x","x","x","x"]  
 ];
+// this is a double win condition for x
 let xdoublewin = [
     ["x","o","x","o","x"],
     ["o","x","o","x","o"],
@@ -16,6 +21,7 @@ let xdoublewin = [
     ["o","x","o","x","o"],
     ["x","o","x","o","x"]
 ];
+// double win condition for o
 let odoublewin = [
     ["o","x","o","x","o"],
     ["x","o","x","o","x"],
@@ -23,6 +29,7 @@ let odoublewin = [
     ["x","o","x","o","x"],
     ["o","x","o","x","o"]
 ];
+// single win condition for x
 let xwin = [
     ["x","x","x","x","x"],
     ["o","x","o","x","o"],
@@ -30,6 +37,7 @@ let xwin = [
     ["x","o","x","o","x"],
     ["x","o","x","o","x"]
 ];
+// single win condition for o
 let owin = [
     ["o","o","o","o","o"],
     ["o","x","o","x","o"],
@@ -37,6 +45,7 @@ let owin = [
     ["x","o","x","o","x"],
     ["x","o","x","o","x"]
 ];
+// array of random values pulled from the possible value array
 let randomarray = [
     [rando,rando,rando,rando,rando],
     [rando,rando,rando,rando,rando],
@@ -46,9 +55,9 @@ let randomarray = [
 ];
 
 //test('tests game state for o winning', () => {
-//    expect(insertgamestateevaluationfunctionhere(owin)).toBe(enumvalueforowinning);
+//    expect(insertgamestateevaluationfunctionhere(owin)).toBe(insertenumvalueforowinning);
 //  });
 //test('tests game state for x winning', () => {
-//    expect(insertgamestateevaluationfunctionhere(xwin)).toBe(enumvalueforxwinning);
+//    expect(insertgamestateevaluationfunctionhere(xwin)).toBe(insertenumvalueforxwinning);
 //  });
 // etc...
