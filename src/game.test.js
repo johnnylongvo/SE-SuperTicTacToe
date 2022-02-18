853d690bc6112cb7fd50bc4a8bf20d61fc28650e
Brent Bashaw <bbashaw@uco.edu>
2022-02-18 09:27:27 -0600

    adding Landon's game class updates

--- src/game.test.js
+++ src/game.test.js
@@ -1,11 +1,9 @@
+//what to do next
+
 // we need to install enumifyjs, use typescript, or homebrew an enum class
-// the following line gets the game class
-const sum = require('./game');
-// this is an array defining potential values for a game square
+const checkWinner = require('./game');
 let xando = ["x","o",""];
-// this pulls a random value from the above array
 var rando = xando[Math.random(Math.floor()*xando.length)];
-// this defines a game whose state should not be possible
 let extraneous = [
     ["x","x","x","x","x"],
     ["o","o","o","o","o"],
@@ -13,7 +11,6 @@ let extraneous = [
     ["o","o","o","o","o"],
     ["x","x","x","x","x"]  
 ];
-// this is a double win condition for x
 let xdoublewin = [
     ["x","o","x","o","x"],
     ["o","x","o","x","o"],
@@ -21,7 +18,6 @@ let xdoublewin = [
     ["o","x","o","x","o"],
     ["x","o","x","o","x"]
 ];
-// double win condition for o
 let odoublewin = [
     ["o","x","o","x","o"],
     ["x","o","x","o","x"],
@@ -29,7 +25,6 @@ let odoublewin = [
     ["x","o","x","o","x"],
     ["o","x","o","x","o"]
 ];
-// single win condition for x
 let xwin = [
     ["x","x","x","x","x"],
     ["o","x","o","x","o"],
@@ -37,7 +32,6 @@ let xwin = [
     ["x","o","x","o","x"],
     ["x","o","x","o","x"]
 ];
-// single win condition for o
 let owin = [
     ["o","o","o","o","o"],
     ["o","x","o","x","o"],
@@ -45,7 +39,6 @@ let owin = [
     ["x","o","x","o","x"],
     ["x","o","x","o","x"]
 ];
-// array of random values pulled from the possible value array
 let randomarray = [
     [rando,rando,rando,rando,rando],
     [rando,rando,rando,rando,rando],
@@ -54,10 +47,24 @@ let randomarray = [
     [rando,rando,rando,rando,rando]
 ];
 
-//test('tests game state for o winning', () => {
-//    expect(insertgamestateevaluationfunctionhere(owin)).toBe(insertenumvalueforowinning);
-//  });
-//test('tests game state for x winning', () => {
-//    expect(insertgamestateevaluationfunctionhere(xwin)).toBe(insertenumvalueforxwinning);
-//  });
+let xWinArr=['x', 'x','x','x','x',
+"o","x","o","x","o",
+"o","x","o","x","o",
+"x","o","x","o","x",
+"x","o","x","o","x"
+]
+
+let oWinArr=['o', 'o','o','o','o',
+"o","x","o","x","o",
+"o","x","o","x","o",
+"x","o","x","o","x",
+"x","o","x","o","x"
+]
+
+test('tests game state for o winning', () => {
+   expect(checkWinner(randomarray)).toBe("o");
+ });
+test('tests game state for x winning', () => {
+   expect(checkWinner(xWinArr)).toBe("x");
+ });
 // etc...
