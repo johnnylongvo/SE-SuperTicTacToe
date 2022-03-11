52dea8fe8f6e8d41fa6af96b161ab219ff65d29b
lmanuel34 <landonmanuel34@yahoo.com>
2022-03-11 08:24:10 -0600

    on click-x and then next click o and minor changes

--- src/App.js
+++ src/App.js
@@ -11,13 +11,48 @@ import './App.css';
 import Card from 'react-bootstrap/Card';
 import React, { useState } from 'react';
 
+
+
 function App() {
+  const [mark, setMark] = useState('');
+  const [matrix, setMatrix] = useState([
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    ]);
+
+  const winningConditions = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24],
+    [0, 5, 10, 15, 20],
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24],
+    [0, 6, 12, 18, 24],
+    [4, 8, 12, 16, 20],
+    ];
+    
   var sectionStyle = {
     backgroundImage: `url(${BGImage})`
   }
 
-  const handleClick = e => {
+  const handleClick = (e, column, row) => {
     console.log(e.target.id);
+    if(mark === 'x'){
+      e.target.innerText = 'o'
+      setMark('o');
+    } else{
+      e.target.innerText = 'x'
+      setMark('x');
+    }
+    
+    
   }
 
   return (
@@ -41,11 +76,23 @@ function App() {
         <div class="row d-flex justify-content-center">
           <table class="table">
             <tbody>
-              {Array.from({ length: 5}).map((_, outerIndex) => (
+              {/* {Array.from({ length: 5}).map((_, outerIndex) => (
                   <tr key={outerIndex}>
                       {Array.from({ length: 5}).map((_, innerIndex) => (
                         <td class="text-center border-2 border-dark border-rounded" key={innerIndex}>
                             <button class="cell btn btn-secondary" id={"btn"+((outerIndex*5)+innerIndex)} onClick={handleClick}>{(outerIndex*5)+innerIndex}</button>
+                        </td>
+                      ))}
+                  </tr>
+              ))} */}
+               {matrix.map((row, outerIndex) => (
+                  <tr key={outerIndex}>
+                      {row.map((column, innerIndex) => (
+                        <td class="text-center border-2 border-dark border-rounded" key={innerIndex}>
+                            <button class="cell btn btn-secondary" id={"btn"+((outerIndex*5)+innerIndex)}
+							onClick={(e) =>
+                    handleClick(e, innerIndex, outerIndex)
+                  }>{(outerIndex*5)+innerIndex}</button>
                         </td>
                       ))}
                   </tr>
